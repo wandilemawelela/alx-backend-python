7b57ce6df5919febb780594c4ae34322c199ae84
wandilemawelela <wandile@gmail.com>
2024-11-04 18:00:34 +0000

    [FIX] Test bugs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 """
-Tests for get_json function
+Tests for access_nested_map function and get_json function
 """
 
 import unittest
 from unittest.mock import patch, Mock
-from utils import get_json
 from parameterized import parameterized
+from utils import access_nested_map, get_json
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(context.exception.args[0], path[-1])
 
 
 class TestGetJson(unittest.TestCase):
@@ -26,7 +45,8 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
 
-        # Ensure the mocked get method was called exactly once with test_url
+        # Ensure the mocked get method was called exactly
+        # once with test_url
         mock_get.assert_called_once_with(test_url)
 
 
